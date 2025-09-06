c377c47abb45fff8b6c3af1b84bb51d8e8b803a8
thephotogenicbug <navinkumarachari@gmail.com>
2025-09-06 15:56:44 +0530

    components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Admin } from "./components/admin/admin";
+import { User } from "./components/user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Admin, User],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

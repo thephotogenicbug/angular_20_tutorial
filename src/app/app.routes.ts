9b57c0b58e17368523833913c6b10a3731c12610
thephotogenicbug <navinkumarachari@gmail.com>
2025-09-08 18:48:31 +0530

    angular router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalExample } from './components/signal-example/signal-example';
+import { User } from './components/user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: Admin,
+  },
+  {
+    path: 'control-flow',
+    component: ControlFlow,
+  },
+  {
+    path: 'data-binding',
+    component: DataBinding,
+  },
+  {
+    path: 'signal',
+    component: SignalExample,
+  },
+  {
+    path: 'user',
+    component: User,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Admin } from "./components/admin/admin";
-import { User } from "./components/user/user";
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
 import { SignalExample } from './components/signal-example/signal-example';
 import { ControlFlow } from './components/control-flow/control-flow';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Admin, User, DataBinding, SignalExample, ControlFlow],
+  imports: [
+    RouterOutlet,
+    Admin,
+    User,
+    DataBinding,
+    SignalExample,
+    ControlFlow,
+    RouterLink,
+    RouterOutlet,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

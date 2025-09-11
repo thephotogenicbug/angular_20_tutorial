ab8fff454c1d09619dae2f3b8b7ffb61991e144e
thephotogenicbug <navinkumarachari@gmail.com>
2025-09-11 15:47:38 +0530

    Angular Post API

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrl: './user.css',
 })
-export class User {
+export class User implements OnInit {
+  userList: any[] = [];
 
+  userObject: any = {
+    userId: 0,
+    emailId: '',
+    password: '',
+    fullName: '',
+    mobileNo: '',
+  };
+
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http
+      .get('https://api.freeprojectapi.com/api/GoalTracker/getAllUsers')
+      .subscribe((res: any) => {
+        this.userList = res;
+      });
+  }
+
+  onSaveUser() {
+    this.http
+      .post('https://api.freeprojectapi.com/api/GoalTracker/register', this.userObject)
+      .subscribe({
+        next: (res) => {
+          alert('User created');
+          this.getUsers();
+        },
+        error: (error) => {
+          alert('Error: ' + error);
+        },
+      });
+  }
 }
